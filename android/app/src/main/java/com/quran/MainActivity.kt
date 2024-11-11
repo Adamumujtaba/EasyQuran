5cd0bd92aca4efe2ef29ff6bc92f2d598db09df3
Mujtaba <mujtaba.adamu@flexisaf.com>
2024-11-11 01:08:07 +0100

    update the app name

--- android/app/src/main/java/com/quran/MainActivity.kt
+++ android/app/src/main/java/com/quran/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "Quran"
+  override fun getMainComponentName(): String = "EasyQuran"
 override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(null)
   }
--- ios/EasyQuranTests/QuranTests.m
+++ ios/EasyQuranTests/QuranTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface QuranTests : XCTestCase
+@interface EasyQuranTests : XCTestCase
 
 @end
 
-@implementation QuranTests
+@implementation EasyQuranTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
